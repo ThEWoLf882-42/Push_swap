816e370dc3bd2641198d076bab3c5ad0f3f91092
ThEWoLf882-42 <agimi@student.1337.ma>
2022-12-07 18:04:54 +0100

    Errors ?!

--- libft/ft_strcmp.c
+++ libft/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: agimi <agimi@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 14:03:54 by agimi             #+#    #+#             */
-/*   Updated: 2022/12/07 14:03:55 by agimi            ###   ########.fr       */
+/*   Updated: 2022/12/07 18:03:48 by agimi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,18 @@
 int	ft_strcmp(const char *s1, const char *s2)
 {
 	unsigned int	a;
+	unsigned int	b;
 
 	a = 0;
-	while ((s1[a] != '\0' || s2[a] != '\0'))
+	b = 0;
+	while ((s1[a] != '\0' || s2[b] != '\0'))
 	{
-		if (s1[a] != s2[a])
-			return ((unsigned char)s1[a] - (unsigned char)s2[a]);
+		if (s1[a] == '+')
+			a++;
+		if (s1[a] != s2[b])
+			return ((unsigned char)s1[a] - (unsigned char)s2[b]);
 		a++;
+		b++;
 	}
 	return (0);
 }
